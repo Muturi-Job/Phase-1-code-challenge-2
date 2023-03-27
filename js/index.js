1d7f04ee914c9a3c1e2f85a9c156fed91516faa6
Job Muturi <muirimuturi17@gmail.com>
2023-03-27 09:08:01 +0300

    Enabled event listeners

--- js/index.js
+++ js/index.js
@@ -1,26 +1,30 @@
-const animalDetails = document.getElementById("animal-details");
-const animalList = document.getElementById('animal-list');
 
-fetch('http://localhost:3000/characters')
-    .then(res => res.json())
-    .then(data => {
-       
-        data.forEach(animal => {
-            const name = document.createElement('li');
-            name.innerHTML = animal.name;
-            animalList.appendChild(name);
-            name.addEventListener('onclick', () => handleDetails(animal));
-            console.log(handleDetails(animal))
-        })
-    
-    function handleDetails() {
-        (animalDetails) => {
-            const {name, image , votes} = animalDetails;
-            animalDetails.innerHTML = `
-            <p>${name}</p>
-            <img src = '${image}' >
-            <p>Total Votes: ${votes} </p>
-            `;
-        }}
-    })
+document.addEventListener('DOMContentLoaded', ()=>{
+    handleGet();
+});
+//link HTML to js
+const animalList = document.getElementById('animal-list')
+const animalName = document.getElementById('animal-name')
+const animalImage = document.getElementById('animal-image')
+const animalVoteCount = document.getElementById('animal-vote-count')
+const animalVotesForm = document.getElementById('votes-form') 
+//Function to fetch the data
+function handleGet() {
+    fetch('http://localhost:3000/characters')
+        .then(res => res.json())
+        .then(data => handleCharacters(data))
+}
+function handleCharacters(data) {
+    data.forEach((data) => {
+        const nameSpan = document.createElement('li')
+        nameSpan.textContent = data.name;
+        animalList.appendChild(nameSpan);
+        nameSpan.addEventListener('click', () =>{
+            animalName.textContent = data.name;
+            animalImage.setAttribute('src', data.image);
+            animalVoteCount.textContent = data.votes;
+        });
+    });
+}                    
 
+   
